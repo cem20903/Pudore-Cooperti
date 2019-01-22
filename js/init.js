e98af11c208d47a0e731a2aaec9f79da92c98f95
David Casas <cem20903@gmail.com>
2019-01-22 12:58:05 +0100

    Antes de borrar comentarios

--- js/init.js
+++ js/init.js
@@ -4,10 +4,20 @@ window.onload = function(){
   var canvas = htmlCanvas.getContext("2d")
  
   var obstacle1 = new Obstacles(400,390,120,50,"obstacle1",canvas)
-  var obstacle2 = new Obstacles(640,450,40,50,"wood",canvas)
-  var obstacle3 = new Obstacles(780,400,50,100,"wood",canvas);
+  //Piramide
+  var obstacle2 = new Obstacles(640,450,40,50,"wood",canvas);
+  var obstacle3 = new Obstacles(680,400,50,100,"wood",canvas);
+  var obstacle4 = new Obstacles(730,370,50,130,"wood",canvas);
+  var obstacle5 = new Obstacles(770,330,50,170,"wood",canvas);
+  var obstacle6 = new Obstacles(820,370,50,130,"wood",canvas);
+  var obstacle7 = new Obstacles(870,400,50,100,"wood",canvas);
+  var obstacle8 = new Obstacles(920,450,40,50,"wood",canvas);
+  var obstacle9 = new Obstacles(1050,390,120,50,"obstacle1",canvas)
 
-  var arrayObstacle = [obstacle1,obstacle2,obstacle3]
+//Fin piramide
+
+  var arrayObstacle = [obstacle1,obstacle2,obstacle3,
+    obstacle4,obstacle5,obstacle6,obstacle7,obstacle8,obstacle9]
   
   var object1 = new Object(440,360,30,30,"coin",canvas)
 
@@ -16,32 +26,66 @@ window.onload = function(){
   var imgCharacter = new Image();
   imgCharacter.src = "images/character.png"
 
+  var interruptor = true;
 
   
   setInterval(function () {
 
     clear()
 
+    if(character.x >= 1400){
+     level.numLevel++
+     character.x = 50;
+    }
+
     loadBackground.initBackground();
     canvas.drawImage(imgCharacter, character.x, character.y, character.width, character.height)
   
+    if(level.numLevel == 1){
     obstacle1.initObstacles()
     object1.initObject()
     obstacle2.initObstacles()
     obstacle3.initObstacles()
+    obstacle4.initObstacles()
+    obstacle5.initObstacles()
+    obstacle6.initObstacles()
+    obstacle7.initObstacles()
+    obstacle8.initObstacles()
+    obstacle9.initObstacles()
+    
+    //if(character.x > 190 && character.x < 230)
+    //alert("Tramp")
+
+  }
+
+  if(level.numLevel == 2){
+    
+  }
+
     colision()
     //Se refresca y permite que exista movimiento
     //Cargamos cada XXX milisegundos
+    
+// if( (obstacle4.x >= 140 && obstacle4.x <= 200) && interruptor){
+//   obstacle4.x++
+// } else{
+//   interruptor = false
+// }
+// if(interruptor == false && obstacle4.x <= 201 && obstacle4.x >140 ){
+//   obstacle4.x--
+// } else {
+//   interruptor = true;
+// }
 
 
 
 
 
     //La function colission comprueba las colisiones.
+comprobar()
 
 
-
-  }, 1000/60)
+}, 1000/60)
 
   function clear(){
     canvas.clearRect(0,0,1400,600)
@@ -49,28 +93,27 @@ window.onload = function(){
   }
 
 
+  function comprobar(){
+   // DA TRUE cuando esta en el mismo eje X console.log(character.x + character.width +3 > obstacle1.x)
 
+   //Funciona con el primer obstaculo
+   if(/* Si el jugador esta por encima de la parte baja del muñeco */
+     character.y < obstacle3.y + obstacle3.height &&
+     character.y + character.height /*-10*/ > obstacle3.y &&
+    character.x + character.width > obstacle3.y &&
+    obstacle3.x + obstacle3.width > character.x
+    ){
+     console.log("sss")
+     character.isPossibleX = false
+   } else {
+     character.isPossibleX = true
+   }
+
+
+
+  }
  
   function colision(){
-    // X left
-    // Y top
-    // Si el personaje no esta en la 450 y no esta en una plataforma
-
-  
-   
-// if(character.x == 410 && character.y == 350){
-  
-
-// }
-
-// Cae el primer obstaculo
-// if(character.x > 410 && character.x < 440 && character.y == 450){
-//  // console.log("Cae")
-//   // var ints = setInterval(function(){
-//   //   obstacle1.y++
-   
-//   // },100)
-// }
 
 if(
   character.x <  object1.x + object1.width &&
@@ -81,9 +124,9 @@ if(
  // Coge la moneda  
   console.log("Colision Moneda")
 }
- 
 
-    if(character.isPlataform == false){
+  
+if(character.isPlataform == false){
       character.inter()
     }
 
@@ -92,85 +135,54 @@ if(
       character.isPlataform = true
     }
 
-    //Primer Obstaculo
-  //  if(character.y == 350 && character.x > 355 && character.x < 500){
-  //    character.isPlataform = true
-  //  }
+ 
 
  
 
 
-   //Para que se caiga por los bordes del primer objeto
-   
-  //  if(character.y == 350 && (character.x <= 350 || character.x > 500 ) ){
-  //   character.isPlataform = false;
-  //  }
 
 
-
-  
-// Primera plataforma
-    // if(character.x <  obstacle1.x + obstacle1.width &&
-    //   obstacle1.x  < character.x + character.width &&
-    //   character.y < obstacle1.y + obstacle1.height &&
-    //   obstacle1.y < character.y + character.height
-    //   ){
-       
-    //     //alert(character.y)
-    //     character.isPossibleX = false;
-    //     //character.isPossibleX = false;
-    //   } else{
-    //     character.isPossibleX = true;
-    //     //character.isPossibleX = true;
-    //   }
-
-
-// Segunda plataforma 
-
-// Para que no atraviese el segundo objeto.
 
 //Tercera plataforma 
-if( (character.x + character.width) > obstacle3.x && character.x < obstacle3.x + obstacle3.width && character.y == 450){
-  character.isPossibleX = false
-} else {
-  character.isPossibleX = true
-}
+// if( (character.x + character.width) > obstacle3.x && character.x < obstacle3.x + obstacle3.width && character.y == 450){
+//   character.isPossibleX = false
+// } else {
+//   character.isPossibleX = true
+// }
+
+
+arrayObstacle.forEach(function(obstacle){
+  
+  
+//   if(character.y < obstacle.y + obstacle.height &&
+//   character.y + character.height  > obstacle.y &&
+//  character.x + character.width > obstacle.y &&
+//  obstacle.x + obstacle.width > character.x){
+//    console.log("asdf")
+//  }
+
+})
 
 // Esto funciona - Segunda plataforma
-if(character.x + character.width > obstacle2.x  && character.x < obstacle2.x+obstacle2.width 
-  && character.y == 450 ){
-  character.isPossibleX = false;
 
-} 
 
 // Volver para atras 2º obstaculo
-if(character.x > obstacle2.x + (obstacle2.width - 5)&&(character.x < obstacle2.x + (obstacle2.width + 5))  && character.y == 450){
+// if(character.x > obstacle2.x + (obstacle2.width - 5)&&(character.x < obstacle2.x + (obstacle2.width + 5))  && character.y == 450){
  
- character.isPossibleReturn = false;
+//  character.isPossibleReturn = false;
 
-} else {
-  character.isPossibleReturn = true;
-} 
+// } else {
+//   character.isPossibleReturn = true;
+// } 
 
 //Volver para atras 3º obstaculo
-if(character.x > obstacle3.x + (obstacle3.width - 5)
-&&(character.x < obstacle3.x + (obstacle3.width + 5)) 
-&& character.y == 450){
+// if(character.x > obstacle3.x + (obstacle3.width - 5)
+// &&(character.x < obstacle3.x + (obstacle3.width + 5)) 
+// && character.y == 450){
 
-  character.isPossibleReturn = false;
+//   character.isPossibleReturn = false;
 
-}
-
-
-//  if(character.x > obstacle2.x){
-//   alert(character.y)
-//  }
-   //Segundo Obstaculo
-  //  if(character.y == 350 && character.x > 355 && character.x < 500){
-  //   character.isPlataform = true
-  // }
-
-  // X left
+// }
 
   if(character.y == 450){
     character.isPlataform = true;
@@ -181,84 +193,20 @@ if(character.x > obstacle3.x + (obstacle3.width - 5)
 
   arrayObstacle.forEach(function(obstacle){
     
-    if( character.y + character.height < obstacle.y 
+
+    //Para poder ANDAR POR ENCIMA
+    if( character.y + character.height > obstacle.y 
       && character.x + character.width > obstacle.x 
       && character.x < obstacle.width + obstacle.x  ){
         character.isPlataform = true
       }
-
-
   })
-  
-  // if( character.y + character.height < obstacle1.y 
-  // //   && character.x + character.width > obstacle1.x 
-  // //   && character.x < obstacle1.width + obstacle1.x  ){
-  // //   character.isPlataform = true;
-    
-    
-  // // } else  if( character.y + character.height == obstacle2.y 
-  // //   && character.x + character.width > obstacle2.x 
-  // //   && character.x < obstacle2.width + obstacle2.x ){
-  // //   character.isPlataform = true;
-    
-  // // } else {
-  // //   character.isPlataform = false;
-  // // } 
-
-
-
- 
-
-  
-
-  
-      // Parametros de primer Obstaculo
-   // if(character.left === 350 && character.top >= 345 && character.top <= 445 ){
-      // Para que no atraviese la parte izquierda
-     // character.isPossibleX = false;
-  
-   // } else {
-   
-// X left
-// Y top
-
-    //  character.isPossibleX = true
-   // }
-
-      
-      // if(character.x >= obstacle1.colissionLeft && 
-      //   character.left <= obstacle1.colissionRight && character.top == 345){
-      //   isPlatform = true
-        
-          
-      //   } else{
-      //     isPlatform = false;
-      //   }
-      
 
   }
 
 
   
-  
 
-  function checkColission(){
-
-    //Position Original 
-
-  //   if(level.numLevel == 1){
-      
-  //       if(character.left >= obstacle1.colissionLeft && 
-  //         character.left <= obstacle1.colissionRight && character.top == 345){
-  //           character.isPossibleJump = true
-
-  //           //Hago clear Interval para que deje de caer.
-  //           clearInterval(character.jumpInterval)
-  //           character.top -= 5
-  //       } 
-  // }
-
-  }
   
   var loadBackground = new Background(canvas);
   loadBackground.initBackground();
@@ -268,7 +216,7 @@ if(character.x > obstacle3.x + (obstacle3.width - 5)
 
   window.addEventListener("keydown",function(e){
 
-    
+    comprobar();
     switch (e.keyCode){
       case 37:
     
